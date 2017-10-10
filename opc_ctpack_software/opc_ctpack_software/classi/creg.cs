b494beaac7d9d2f143498505016a79ca57a9b646
Tibe93 <francesco.tiberia@gmail.com>
2017-10-10 14:52:32 +0200

    c

---
+++ opc_ctpack_software/opc_ctpack_software/classi/creg.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace OPC_CTPACK_Software
+{
+    public class Creg
+    {
+        Formato Formato;
+        double[] PosConv;
+        double[] VelConv;
+        double[] CorrConv;
+        double[] Time;
+        int Periodi;
+        public double VelocitaMedia;
+        public double Velocita2RMS;
+        public double CregAttuale;
+
+        public Creg(Formato Formato, string Path)
+        {
+            this.Formato = Formato;
+            File.OpenRead("C:\Users\CtPack\Desktop\Tiberia\Trend_Tibe\_Fossalta_Temperature_Termoregolate\1");
+        }
+    }
+}
