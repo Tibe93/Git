6b2dd5dd64efb92f5b58c67c6a0f59e8d46c0509
Tibe93 <francesco.tiberia@gmail.com>
2017-10-11 09:40:48 +0200

    cc

--- opc_ctpack_software/opc_ctpack_software/classi/creg.cs
+++ opc_ctpack_software/opc_ctpack_software/classi/creg.cs
@@ -29,18 +29,36 @@ namespace OPC_CTPACK_Software
             StreamReader Csv = new StreamReader(Pathh);
             string a = Csv.ReadLine(); //riga 1
             string b = Csv.ReadLine(); //riga 2, i dati iniziano alla riga 3
-
+            
             ArrayList x = new ArrayList();
+            string[] y;
+            ArrayList Tempo = new ArrayList();
+            ArrayList Pos = new ArrayList(); 
+            ArrayList Vel = new ArrayList(); 
+            ArrayList Cor = new ArrayList();
+            int i = 0;
             while (!Csv.EndOfStream)
             {
                 x.Add(Csv.ReadLine());
+                y = x[i].ToString().Split(',');
+                Tempo.Add(double.Parse(y[0]));
+                Pos.Add(double.Parse(y[1].Trim('"').Replace('.',',')));
+                Vel.Add(double.Parse(y[2].Trim('"').Replace('.', ',')));
+                Cor.Add(double.Parse(y[3].Trim('"').Replace('.', ',')));
+                i++;
             }
+
+
+            Console.ReadLine();
+            /* Questa porzione di codice è stata utilizzata per modificare un .CSV di quelli vecchi in modo da crearne uno attuale
             int i = 0;
             ArrayList y = new ArrayList();
             StreamWriter Css = new StreamWriter($"{Path}test.CSV");
+            string[] appoggio;
             for(i = 0; i <3000; i++)
             {
-                y.Add($"{4*i},{ x[i].ToString().Substring(29,35)}");//!!devo fare il parsing utilizzando la formattazione sennò non va
+                appoggio = x[i].ToString().Split(',');
+                y.Add($"{4 * i}\t{appoggio[1].Trim('"').Replace('.',',')}\t{appoggio[2].Trim('"').Replace('.', ',')}\t{appoggio[3].Trim('"').Replace('.', ',')}");//ToString().Substring(29,35)}");
             }
             Console.ReadLine();
             Css.WriteLine(a);
@@ -50,6 +68,7 @@ namespace OPC_CTPACK_Software
                 Css.WriteLine(s);
             }
             Css.Close();
+            */
         }
     }
 }
