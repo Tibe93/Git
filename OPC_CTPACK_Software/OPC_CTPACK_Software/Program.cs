ab3ad3e845647908a7073854b060a2aae2681921
Alessandro Gadda <gadda.alessandro@gmail.com>
2017-10-11 11:51:29 +0200

    aa

--- OPC_CTPACK_Software/OPC_CTPACK_Software/Program.cs
+++ OPC_CTPACK_Software/OPC_CTPACK_Software/Program.cs
@@ -16,12 +16,6 @@ namespace OPC_CTPACK_Software
         [STAThread]
         static void Main()
         {
-
-            double[,] VelocitaMediaTot = new double[2, 5] { { 1,1,1,1,1 }, { 2,2,2,2,2 }};
-            double[,] Velocita2RMSTot = new double[5, 1] { { 1 }, { 1 }, { 1 }, { 1 }, { 1 } };
-            double[,] Attriti = new double[2, 1];
-            Attriti = Functions.MultiplyMatrix(VelocitaMediaTot, Velocita2RMSTot, 2, 4, 0);
-
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
