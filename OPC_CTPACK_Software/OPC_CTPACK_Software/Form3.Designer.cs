2e8b259dfbee09e2c7b64a489de91fc0bbb14f80
Alessandro Gadda <gadda.alessandro@gmail.com>
2017-10-12 14:15:50 +0200

    14:15

--- OPC_CTPACK_Software/OPC_CTPACK_Software/Form3.Designer.cs
+++ OPC_CTPACK_Software/OPC_CTPACK_Software/Form3.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(760, 750);
+            this.Name = "Form3";
             this.Text = "Form3";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
